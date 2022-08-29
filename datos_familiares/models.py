1d20606b0f0366cb5dd98bc616c641801c336ed2
Luca Garavelli <lucagaravelli10@gmail.com>
2022-08-28 23:55:17 -0300

    Cree el proyecto en Django, la app y el modelo de la misma. Además se realizaron las migraciones a la Base de Datos

---
+++ datos_familiares/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class familiares(models.Model):
+    nombre = models.CharField(max_length=64)
+    apellido = models.CharField(max_length=64)
+    fecha_nacimiento = models.DateField()
+    peso_kg = models.IntegerField()
